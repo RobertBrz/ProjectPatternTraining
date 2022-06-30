6e5e8de57c4f12b4ade62328c1e959b05f630c6e
rob.brz <robert.brzozowski@ftsgps.com>
2022-06-30 12:43:48 +0200

    Add project files.

---
+++ ProjectPatternTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectPatternTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
